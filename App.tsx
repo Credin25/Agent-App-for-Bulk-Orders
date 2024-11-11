d1242d7312075c75ea7213aa7e431081b5152e74
kavya-credin <kavya.rajput@credin.in>
2024-11-11 17:53:57 +0530

    change the logo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import ShopScreen from './screens/ShopScreen';
-import SettingsScreen from './screens/SettingsScreen';
 import BillingScreen from './screens/BillingScreen';
 import StoreScreen from './screens/StoreScreen';
 import MatIcon from 'react-native-vector-icons/MaterialIcons';
 import LoginScreen from './screens/LoginScreen';
 import InsuranceStack from './Navigation/Insurance';
-import { useSelector } from 'react-redux';
+import SettingsStack from './Navigation/Setting';
+import { useSelector, useDispatch } from 'react-redux';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import APIroute from './constants/route';
+import axios from 'axios';
+import { login } from './reducers/userSlice';
 
 const Tab = createBottomTabNavigator();
-
-// Move tabBarIcon outside the component
 const getTabBarIcon = (routeName: string) => {
   let iconName;
 
@@ -22,15 +24,16 @@ const getTabBarIcon = (routeName: string) => {
     case 'Home':
       iconName = 'home';
       break;
-    case 'Shop':
+    case 'E-com Shop':
       iconName = 'shopping-cart';
       break;
     case 'Insurance':
       iconName = 'local-hospital';
-    case 'Store':
+      break; 
+    case 'My Store':
       iconName = 'store';
       break;
-    case 'Billing':
+    case 'E-com Billing':
       iconName = 'calculate';
       break;
     case 'Settings':
@@ -43,43 +46,62 @@ const getTabBarIcon = (routeName: string) => {
   return (color: string, size: number) => <MatIcon name={iconName} size={size} color={color} />;
 };
 
-
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector((state: any) => state.user.user);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const checkStatus = async () => {
+      const accessToken = await AsyncStorage.getItem('accessToken');
+      const refreshToken = await AsyncStorage.getItem('refreshToken');
+      if (accessToken && refreshToken) {
+        fetchUserData(accessToken, refreshToken);
+      }
+    };
+
+    const fetchUserData = async (accessToken: string, refreshToken: string) => {
+      try {
+        const response = await axios.get(`${APIroute}/agent/user`, {
+          headers: {
+            authorization: accessToken,
+            refreshtoken: refreshToken,
+          },
+        });
+        dispatch(login(response.data.data));
+        setIsLoggedIn(true);
+      } catch (error) {
+        console.error('Failed to fetch user data:', error);
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkStatus();
+  }, [dispatch]);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-
-
       <NavigationContainer>
         <Tab.Navigator
           screenOptions={({ route }) => ({
             headerShown: false,
-            tabBarIcon: ({ color, size }) => getTabBarIcon(route.name)(color, size),
-            tabBarActiveTintColor: '#044ca4',
+            tabBarLabel: route.name === 'Login' ? () => null : undefined,
+            tabBarIcon: route.name === 'Login' ? () => null : ({ color, size }) => getTabBarIcon(route.name)(color, size),
+            tabBarActiveTintColor: '#0e4985',
             tabBarInactiveTintColor: 'gray',
           })}
         >
           {user ? (
             <>
               <Tab.Screen name="Home" component={HomeScreen} />
-              <Tab.Screen
-                name="Insurance"
-                component={InsuranceStack}
-                options={({ route }) => ({
-                  headerShown: false,
-                })}
-              />
-              {/* <Tab.Screen name="Insurance" component={InsuranceOptions} /> */}
-              <Tab.Screen name="Shop" component={ShopScreen} />
-              <Tab.Screen name="Store" component={StoreScreen} />
-              <Tab.Screen name="Billing" component={BillingScreen} />
-              <Tab.Screen name="Settings" component={SettingsScreen} />
+              <Tab.Screen name="Insurance" component={InsuranceStack} />
+              <Tab.Screen name="E-com Shop" component={ShopScreen} />
+              <Tab.Screen name="My Store" component={StoreScreen} />
+              <Tab.Screen name="E-com Billing" component={BillingScreen} />
+              <Tab.Screen name="Settings" component={SettingsStack} />
             </>
           ) : (
             <Tab.Screen name="Login" component={LoginScreen} />
           )}
-
         </Tab.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
